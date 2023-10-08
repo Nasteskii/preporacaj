50573b9ef3823e3358f568922d59344d02173eaa
Nasteskii <borce.nasteski123@gmail.com>
2023-10-08 16:07:05 +0200

    update side menu style

--- preporacaj_frontend/src/components/containerComponent/ContainerComponent.tsx
+++ preporacaj_frontend/src/components/containerComponent/ContainerComponent.tsx
@@ -1,7 +1,12 @@
 import SideMenuComponent from "../sideMenuComponent/SideMenuComponent";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function ContainerComponent() {
-  return <SideMenuComponent />;
+  return (
+    <Router>
+      <SideMenuComponent />;
+    </Router>
+  );
 }
 
 export default ContainerComponent;
--- preporacaj_frontend/src/components/sideMenuComponent/SideMenuComponent.tsx
+++ preporacaj_frontend/src/components/sideMenuComponent/SideMenuComponent.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 
@@ -26,6 +26,7 @@ import ModalComponent from "../modalComponent/ModalComponent";
 function SideMenuComponent() {
   const [menuCollapse, setMenuCollapse] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const location = useLocation();
 
   const menuIconClick = () => {
     setMenuCollapse((prevCollapse) => !prevCollapse);
@@ -39,81 +40,103 @@ function SideMenuComponent() {
     setShowModal(false);
   };
 
+  const getMenuItemClass = (route: string) => {
+    return (
+      "mt-24 w-fit " +
+      (location.pathname === route ? "border-2 rounded-2xl" : "")
+    );
+  };
+
   return (
     <div className="flex no-wrap h-screen w-screen">
-      <Router>
-        <div className={menuCollapse ? "w-20" : "w-64"}>
-          <Sidebar
-            className="h-screen w-full min-w-full sidebar bg-metal"
-            collapsed={menuCollapse}
-          >
-            <Menu>
-              <MenuItem
-                active={true}
-                icon={menuCollapse ? <FiHome /> : null}
-                component={<Link to="/home" />}
-                className="menu-item mt-5"
-              >
-                <h2 className="text-3xl text-center p-3">Препорачај</h2>
+      <div className={menuCollapse ? "w-20" : "w-64"}>
+        <Sidebar
+          className="h-screen w-full min-w-full bg-metal sidebar"
+          collapsed={menuCollapse}
+        >
+          <Menu>
+            <MenuItem
+              active={true}
+              icon={menuCollapse ? <FiHome /> : null}
+              component={<Link to="/home" />}
+              className="menu-item mt-5"
+            >
+              <h2 className="text-3xl text-center p-3">Препорачај</h2>
+            </MenuItem>
+            <div
+              className="text-black absolute right-3.5 z-10 font-bold text-xl top-20 cursor-pointer w-14 min-w-14"
+              onClick={menuIconClick}
+            >
+              {menuCollapse ? <FiArrowRightCircle /> : <FiArrowLeftCircle />}
+            </div>
+            <MenuItem
+              icon={<FiHeart />}
+              component={<Link to="/my-recommendations" />}
+              className={getMenuItemClass("/my-recommendations")}
+            >
+              Мои препораки
+            </MenuItem>
+            <MenuItem
+              icon={<RiCarLine />}
+              component={<Link to="/vehicles" />}
+              className={
+                location.pathname === "/vehicles" ? "border-2 rounded-2xl" : ""
+              }
+            >
+              Возила
+            </MenuItem>
+            <MenuItem
+              icon={<MdMicrowave />}
+              component={<Link to="/home-appliances" />}
+              className={
+                location.pathname === "/home-appliances"
+                  ? "border-2 rounded-2xl"
+                  : ""
+              }
+            >
+              Домаќинство
+            </MenuItem>
+            <MenuItem
+              icon={<FiBookOpen />}
+              component={<Link to="/books" />}
+              className={
+                location.pathname === "/books" ? "border-2 rounded-2xl" : ""
+              }
+            >
+              Книги
+            </MenuItem>
+            <MenuItem
+              icon={<MdOutlineDesktopWindows />}
+              component={<Link to="/IT" />}
+              className={
+                location.pathname === "/IT" ? "border-2 rounded-2xl" : ""
+              }
+            >
+              ИТ и компјутери
+            </MenuItem>
+            <div className="mt-56">
+              <MenuItem icon={<FiLogOut />} onClick={() => logOut()}>
+                Одјави се
               </MenuItem>
-              <div
-                className="text-black absolute right-3.5 z-10 font-bold text-xl top-20 cursor-pointer w-14 min-w-14"
-                onClick={menuIconClick}
-              >
-                {menuCollapse ? <FiArrowRightCircle /> : <FiArrowLeftCircle />}
-              </div>
-              <MenuItem
-                icon={<FiHeart />}
-                component={<Link to="/my-recommendations" />}
-                className="mt-24"
-              >
-                Мои препораки
-              </MenuItem>
-              <MenuItem
-                icon={<RiCarLine />}
-                component={<Link to="/vehicles" />}
-              >
-                Возила
-              </MenuItem>
-              <MenuItem
-                icon={<MdMicrowave />}
-                component={<Link to="/home-appliances" />}
-              >
-                Домаќинство
-              </MenuItem>
-              <MenuItem icon={<FiBookOpen />} component={<Link to="/books" />}>
-                Книги
-              </MenuItem>
-              <MenuItem
-                icon={<MdOutlineDesktopWindows />}
-                component={<Link to="/IT" />}
-              >
-                ИТ и компјутери
-              </MenuItem>
-              <div className="mt-56">
-                <MenuItem icon={<FiLogOut />} onClick={() => logOut()}>
-                  Одјави се
-                </MenuItem>
-              </div>
-            </Menu>
-          </Sidebar>
-          {showModal && (
-            <ModalComponent
-              modalText="Дали сте сигурни?"
-              closeModal={closeModal}
-            />
-          )}
-        </div>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/my-recommendations" element={<MyRecommendations />} />
-          <Route path="/vehicles" element={<Vehicles />} />
-          <Route path="/home-appliances" element={<HomeAppliances />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/IT" element={<IT />} />
-          <Route path="/logout" />
-        </Routes>
-      </Router>
+            </div>
+          </Menu>
+        </Sidebar>
+        {showModal && (
+          <ModalComponent
+            modalText="Дали сте сигурни?"
+            closeModal={closeModal}
+          />
+        )}
+      </div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/my-recommendations" element={<MyRecommendations />} />
+        <Route path="/vehicles" element={<Vehicles />} />
+        <Route path="/home-appliances" element={<HomeAppliances />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/IT" element={<IT />} />
+        <Route path="/logout" />
+      </Routes>
     </div>
   );
 }
