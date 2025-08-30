9594612231aa96af5c62e2e0b21d77614a1dc18c
Borche Nasteski <86986540+Nasteskii@users.noreply.github.com>
2025-08-30 17:21:23 +0200

    Update recommendation model and service, update security configuration, configure token blacklist (#11)

--- preporacaj_backend/src/main/java/com/preporacaj/preporacaj_backend/controller/ProfileController.java
+++ preporacaj_backend/src/main/java/com/preporacaj/preporacaj_backend/controller/ProfileController.java
@@ -1,8 +1,10 @@
 package com.preporacaj.preporacaj_backend.controller;
 
 import com.preporacaj.preporacaj_backend.model.Profile;
+import com.preporacaj.preporacaj_backend.model.response.ProfileResponse;
 import com.preporacaj.preporacaj_backend.service.ProfileService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/profiles")
@@ -21,17 +24,36 @@ public class ProfileController {
 
     @PreAuthorize("hasRole('SUPERUSER')")
     @GetMapping
-    public ResponseEntity<List<Profile>> allProfiles() {
-        List<Profile> profiles = profileService.allProfiles();
+    public ResponseEntity<List<ProfileResponse>> allProfiles() {
+        try {
+            List<Profile> profiles = profileService.allProfiles();
+            List<ProfileResponse> profileResponses =
+                    profiles.stream().map(profile -> new ProfileResponse(
+                                    profile.getId(), profile.getEmail(),
+                                    profile.getName(), profile.getSurname())
+                            )
+                            .toList();
 
-        return ResponseEntity.ok(profiles);
+            return new ResponseEntity<>(profileResponses, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/me")
-    public ResponseEntity<Profile> authenticatedProfile() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Profile currentProfile = (Profile) authentication.getPrincipal();
+    public ResponseEntity<ProfileResponse> authenticatedProfile() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Profile currentProfile = (Profile) authentication.getPrincipal();
 
-        return ResponseEntity.ok(currentProfile);
+            ProfileResponse profileResponse = new ProfileResponse(
+                    currentProfile.getId(), currentProfile.getEmail(),
+                    currentProfile.getName(), currentProfile.getSurname()
+            );
+
+            return new ResponseEntity<>(profileResponse, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 }
